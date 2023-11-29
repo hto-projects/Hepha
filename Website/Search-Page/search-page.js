ce1322b5bcf0938a51e85f642068921a13547e2c
Jason Bucsanyi <jason.bucsanyi@hyland.com>
2023-11-29 12:06:00 -0500

    added type to button to explicitly state it's a button

--- Website/Search-Page/search-page.js
+++ Website/Search-Page/search-page.js
@@ -1,15 +1,5 @@
-// var testAPI = {
-//   method: 'GET',
-//   redirect: 'follow'
-// };
-
-
-
-
 let url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?";
 const api_key = "&" + "api_key=jJVXuQPnY9fo5Dntk6ReZ7g6h71zLlmnShYmuSHQ";
-
-
 
 function getResults() {
   var requestOptions = {
@@ -17,14 +7,12 @@ function getResults() {
     redirect: 'follow'
   };
 
-  // fetch(buildQuery(), requestOptions)
-  // .then(response => response.json())
-  // .then(result => console.log(result.results[0].id))
-  // .catch(error => console.log('error', error));
-
   fetch(buildQuery(), requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
+    .then(response => response.json())
+    .then(result => {
+      console.log("after call")
+      console.log(result)
+    })
     .catch(error => console.log('error', error));
 }
 
@@ -32,11 +20,7 @@ function getResults() {
 
 function buildQuery() {
     var location_value = document.getElementById('location_input').value
-
     school_location = "&" + "school.state=" + location_value
-    console.log(location_value)
-    console.log(school_location)
-
 
     per_page = "&" + "per_page=" + "3";
 
@@ -44,10 +28,8 @@ function buildQuery() {
 
     query_results = url + fields + per_page  + school_location + api_key;
 
-   
-
     console.log(query_results);
-
+    
     return query_results;
 }
 
