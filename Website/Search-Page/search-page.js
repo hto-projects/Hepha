cb636c6c607ce56dff2857e5e846b52572fdbea1
clarajones4191 <c.jones4191@lakewoodcityschools.org>
2023-12-05 19:24:47 -0500

    filter by school type added

--- Website/Search-Page/search-page.js
+++ Website/Search-Page/search-page.js
@@ -26,11 +26,26 @@ function buildQuery() {
     var population_max = document.getElementById('population_max_input').value
     school_population = "&" + "latest.student.size__range=" + population_min + ".." + population_max
 
+// public = 1, private = 2
+    var school_type_input = document.getElementById("type-of-school-selection");
+    var selected = school_type_input.options[school_type_input.selectedIndex]
+
+    var selected_translated = '1'
+
+    if(selected=="Public") {
+      selected_translated="1"
+    }
+    else if(selected=="Private") {
+      selected_translated="2"
+    }
+
+    school_type = "&" + "school.ownership=" + selected_translated;
+
     per_page = "&" + "per_page=" + "20";
 
-    fields = "fields=" + "id" + "," + "school.name" + "," + "latest.student.size" + "," + "school.state" + "," + "school.ft_faculty_rate" + "," + "school.peps_ownership" + "," + "school.price_calculator_url";
+    fields = "fields=" + "id" + "," + "school.name" + "," + "latest.student.size" + "," + "school.state" + "," + "school.ft_faculty_rate" + "," + "school.ownership" + "," + "school.price_calculator_url";
 
-    query_results = url + fields + per_page  + school_location + school_population + api_key;
+    query_results = url + fields + per_page + school_location + school_population + school_type + api_key;
 
     console.log(query_results);
     
